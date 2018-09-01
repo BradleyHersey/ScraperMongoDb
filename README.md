5334ee473f152270583703eb0909feafd2e63b71
Brad <bradleyhersey00@gmail.com>
2018-09-01 12:25:36 -0400

    serverANDfolder/filesDone

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# ScraperMongoDb
+# ScraperMongoDb  https://bradleyhersey.github.io/ScraperMongoDb/
+express
+express-handlebars
+mongoose
+body-parser
+cheerio
+request
---
+++ server.js
@@ -0,0 +1,38 @@
+// dependencies
+const express = require("express");
+const mongoose = require("mongoose");
+const expressHandlebars = require("express-handlebars");
+const bodyParser = require("body-parser");
+//port 
+const PORT = process.env.PORT || 3100;
+//express app
+const app = express();
+// express router
+const router = express.Router();
+// public folder as a static directory
+app.use(express.static(`${__dirname}/public`));
+//connect handlebars to express
+app.engine("handlebars", expressHandlebars({
+    defaultLayout: "main"
+}));
+app.set("view engine", "handlebars");
+//body Parser 
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+//request go through our router middleware
+app.use(router);
+//use database or local
+const db = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+// logs errors 
+mongoose.connect(db, (error) => {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log("You are connected to mongoose Dude!!!")
+    }
+});
+//listen on the port
+app.listen(PORT, () => {
+    console.log(`Listening on port: ${PORT}`);
+});
